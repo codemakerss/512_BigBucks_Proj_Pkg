17dbbccbb92032807f52c34b378c4bd143462763
Jeff <51349563+codemakerss@users.noreply.github.com>
2023-04-02 09:04:52 -0400

    test file updates

---
+++ tests/test_bigbucks_db.py
@@ -0,0 +1,41 @@
+import sys
+sys.path.append('../src') 
+
+import bigbucks_db as bk
+
+STOCK_API_KEYS = "9Q91BWGMOE13WOR3"
+# Enter database url and keys here
+SUPABASE_URL = 
+KEYS = 
+# db = bk.Table_Updates(SUPABASE_URL, KEYS, STOCK_API_KEYS)
+
+# objs = bk.Buy_And_Sell("9Q91BWGMOE13WOR3")
+# price = objs.get_realtime_stock_price("IBM")
+# print(price)
+
+# test = db.update_transaction_records("Jeffd", "buy", "IBM", 100, price)
+# print(test)
+
+# connect = db.supabase_insert_function(test[0], test[1])
+
+import time
+start_time = time.time()
+#ob2 = bk.Stock_Data(SUPABASE_URL, KEYS, STOCK_API_KEYS)
+# data = ob2.get_stock_information("IBM")
+# ob2.update_stock_info(data)
+
+# data2 = ob2.update_all_stock_price()
+# print(data2)
+# print(ob2.check_symbol_exist("AAPL"))
+
+data3 = bk.Table_View(SUPABASE_URL,KEYS)
+d3 = data3.view_table_data("Transaction_Records")
+print(d3)
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+print(f"Elapsed time: {elapsed_time:.2f} seconds")
+
+
+
+
