ac8a21c7b0f545e1499837eb2e01b1398b59a1bd
Jeff <51349563+codemakerss@users.noreply.github.com>
2023-04-05 22:23:47 -0400

    update tests

--- tests/test_bigbucks_db.py
+++ tests/test_bigbucks_db.py
@@ -7,6 +7,7 @@ STOCK_API_KEYS = "9Q91BWGMOE13WOR3"
 # Enter database url and keys here
 SUPABASE_URL = ""
 KEYS = ""
+
 # db = bk.Table_Updates(SUPABASE_URL, KEYS, STOCK_API_KEYS)
 
 # objs = bk.Buy_And_Sell("9Q91BWGMOE13WOR3")
@@ -29,8 +30,14 @@ start_time = time.time()
 # print(ob2.check_symbol_exist("AAPL"))
 
 data3 = bk.Table_View(SUPABASE_URL,KEYS)
-d3 = data3.view_table_data("Transaction_Records")
+d3 = data3.view_customer_portfolio("Jeffd")
 print(d3)
+# data4 = bk.SP500(SUPABASE_URL, KEYS, STOCK_API_KEYS)
+# d4 = data4.realtime_sp500()
+# print(d4)
+# test = bk.Buy_And_Sell(STOCK_API_KEYS)
+# print(test.realtime_price_bkp("MSFT"))
+
 end_time = time.time()
 elapsed_time = end_time - start_time
 
