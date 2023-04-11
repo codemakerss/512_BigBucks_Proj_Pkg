391cd77401a6053923c77ec2293abf6bdad50cd9
Jeff <51349563+codemakerss@users.noreply.github.com>
2023-04-10 20:30:32 -0400

    fix

--- tests/test_bigbucks_db.py
+++ tests/test_bigbucks_db.py
@@ -7,11 +7,13 @@ STOCK_API_KEYS = "9Q91BWGMOE13WOR3"
 # Enter database url and keys here
 # SUPABASE_URL = ""
 # KEYS = ""
-SUPABASE_URL = 'https://lhjpufbcymwhprgzfbwt.supabase.co'
-KEYS = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoanB1ZmJjeW13aHByZ3pmYnd0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3OTYwNjcwMywiZXhwIjoxOTk1MTgyNzAzfQ.ooR4I6rIpWGKB0kGHIw_PuJfhgqyGnRQ4IDQeznGPpw"
 
-# db = bk.Table_Updates(SUPABASE_URL, KEYS, STOCK_API_KEYS)
+import time
+start_time = time.time()
 
+db = bk.Table_Updates(SUPABASE_URL, KEYS, STOCK_API_KEYS)
+a = db.update_5_year_data()
+print()
 # objs = bk.Buy_And_Sell("9Q91BWGMOE13WOR3")
 # price = objs.get_realtime_stock_price("IBM")
 # print(price)
@@ -21,8 +23,6 @@ KEYS = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx
 
 # connect = db.supabase_insert_function(test[0], test[1])
 
-import time
-start_time = time.time()
 #ob2 = bk.Stock_Data(SUPABASE_URL, KEYS, STOCK_API_KEYS)
 # data = ob2.get_stock_information("IBM")
 # ob2.update_stock_info(data)
@@ -31,9 +31,9 @@ start_time = time.time()
 # print(data2)
 # print(ob2.check_symbol_exist("AAPL"))
 
-data3 = bk.Table_View(SUPABASE_URL,KEYS)
-d3 = data3.view_table_data("SP500_Index")
-print(d3)
+# data3 = bk.Table_View(SUPABASE_URL,KEYS)
+# d3 = data3.view_symbol_price_data("AAPL")
+# print(d3)
 # data4 = bk.SP500(SUPABASE_URL, KEYS, STOCK_API_KEYS)
 # d4 = data4.realtime_sp500()
 # print(d4)
